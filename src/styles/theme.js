cc0e4c420e144dccfea0fab0c1e981e400d24428
RafaelCasemiro <rafindamassa@gmail.com>
2024-06-10 00:44:17 +0000

    segundo commit

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+
+    homeBackground: "#131518",
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, 232A34 0%, #181E27 100%)",
+    textColor: "#969FAD",
+    submitButtomColor: "#FC7614",
+    white: "#fff",
+    feedbackButtomColor: "#262E38",
+    feedbackButtomText: "#7C8798"
+}
