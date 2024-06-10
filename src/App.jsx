075eb5cf58ee634243d87da8e9c17dbf3c20075a
RafaelCasemiro <rafindamassa@gmail.com>
2024-06-10 02:48:39 +0000

    style: posiconamento do card no centro

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 
 import { Home } from "./pages/Home"
-import { Theme } from "./styles/theme.js"
 import { GlobalStyle } from "./styles/global.js"
+import { theme } from "./styles/theme.js"
 
 const router = createBrowserRouter([
 {
@@ -16,8 +16,8 @@ const router = createBrowserRouter([
 export  function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle/>
       <RouterProvider router={router} />
+      <GlobalStyle/>
       </ThemeProvider>
   )
 }
